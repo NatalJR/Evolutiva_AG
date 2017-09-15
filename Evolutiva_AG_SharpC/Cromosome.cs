8d004612fdedc91baa171a9d7d3a7512c867a47a
Natal Junior <juniortl2005@gmail.com>
2017-09-15 18:12:08 -0300

    firstao

---
+++ Evolutiva_AG_SharpC/Cromosome.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Evolutiva_AG_SharpC{
+    class Cromosome{
+        BitArray _gene1;
+        BitArray _gene2;
+        int _fitness;
+
+        public BitArray Gene1 { get => _gene1; set => _gene1 = value; }
+        public BitArray Gene2 { get => _gene2; set => _gene2 = value; }
+        public int Fitness { get => _fitness; set => _fitness = value; }
+
+        public void Initialize()
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < Gene1.Count; i++)
+            {
+                Gene1[i] = rnd.Next(0, 100) > 80 ? true : false;
+                Gene2[i] = rnd.Next(0, 100) > 80 ? true : false;
+            }
+            Evaluate();
+        }
+
+        public int Evaluate()
+        {
+            Fitness=0;
+            for (int i = 0; i < Gene1.Count; i++)
+            {
+                if (Gene1[i])
+                {
+                    Fitness++;
+                }
+                if (Gene2[i])
+                {
+                    Fitness++;
+                }
+            }
+            return Fitness;
+        }
+
+    }
+}
--- Evolutiva_AG_SharpC/Program.cs
+++ Evolutiva_AG_SharpC/Program.cs
@@ -1,15 +1,51 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Evolutiva_AG_SharpC
-{
-    class Program
-    {
-        static void Main(string[] args)
+namespace Evolutiva_AG_SharpC {
+    class Program {
+        static void Main(string[] args) {
+            Cromosome cromosome = new Cromosome();
+            
+            cromosome.Gene1 = new BitArray(256);
+            cromosome.Gene2 = new BitArray(256);
+            
+            //Populacao inicial aleatoria + Fitness inicial
+            cromosome.Initialize();
+
+            //Console.WriteLine(cromosome.Fitness);
+            //PrintGene(cromosome.Gene1, 32);
+            //PrintGene(cromosome.Gene2, 32);
+
+            Console.WriteLine("Pressione uma tecla");
+            Console.ReadKey();
+
+        }
+
+        public static void PrintGene(BitArray gene, int myWidth)
         {
+            int i, width=myWidth;
+            Console.WriteLine("Gene: ");
+            for (i = 0; i < gene.Count; i++, width--)
+            {
+                if (width == 0)
+                {
+                    width = myWidth;
+                    Console.WriteLine();
+                }
+                if (gene[i] == true){
+                    Console.Write("1 ");
+                }
+                else{
+                    Console.Write("0 ");
+                }
+                
+            }
+            Console.WriteLine();
         }
     }
+
 }
