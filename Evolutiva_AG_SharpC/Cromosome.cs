c7a41b6468fe8e683bc3d6bcbd4f9cbc51e67095
NatalJR <juniortl2005@gmail.com>
2017-09-19 04:50:25 -0300

    Almost there

--- Evolutiva_AG_SharpC/Cromosome.cs
+++ Evolutiva_AG_SharpC/Cromosome.cs
@@ -9,16 +9,29 @@ namespace Evolutiva_AG_SharpC{
     class Cromosome{
         BitArray _gene1;
         BitArray _gene2;
+        int _size;
         int _fitness;
 
         public BitArray Gene1 { get => _gene1; set => _gene1 = value; }
         public BitArray Gene2 { get => _gene2; set => _gene2 = value; }
         public int Fitness { get => _fitness; set => _fitness = value; }
+        public int Size { get => _size; set => _size = value; }
 
-        public void Initialize()
-        {
+        public Cromosome(){
+            Size = 512;
+            Gene1 = new BitArray(256);
+            Gene2 = new BitArray(256);
+        }
+
+        public Cromosome(int size){
+            Size = size;
+            Gene1 = new BitArray(size / 2);
+            Gene2 = new BitArray(size / 2);
+        }
+
+        public void Initialize(){
             Random rnd = new Random();
-            for (int i = 0; i < Gene1.Count; i++)
+            for (int i = 0; i < Gene1.Length; i++)
             {
                 Gene1[i] = rnd.Next(0, 100) > 80 ? true : false;
                 Gene2[i] = rnd.Next(0, 100) > 80 ? true : false;
@@ -26,22 +39,17 @@ namespace Evolutiva_AG_SharpC{
             Evaluate();
         }
 
-        public int Evaluate()
-        {
+        public int Evaluate(){
             Fitness=0;
-            for (int i = 0; i < Gene1.Count; i++)
-            {
-                if (Gene1[i])
-                {
+            for (int i = 0; i < Gene1.Count; i++){
+                if (Gene1[i]){
                     Fitness++;
                 }
-                if (Gene2[i])
-                {
+                if (Gene2[i]){
                     Fitness++;
                 }
             }
             return Fitness;
         }
-
     }
 }
--- Evolutiva_AG_SharpC/Program.cs
+++ Evolutiva_AG_SharpC/Program.cs
@@ -8,23 +8,148 @@ using System.Threading.Tasks;
 namespace Evolutiva_AG_SharpC {
     class Program {
         static void Main(string[] args) {
-            Cromosome cromosome = new Cromosome();
-            
+            int popSize = 10;
+            int generations = 0, generationsWithoutImprovement = 0,
+                totalFitness = 0, sorteio = 0;
+            List<Cromosome> population = new List<Cromosome>();
+            List<Cromosome> descendents = new List<Cromosome>();
+            int[] accFitness = new int[popSize];
+            int[,] pairs = new int[popSize / 2, 2];
+            Random rnd = new Random();
+
+            GenerateInitialPopulation(population, popSize);
+            GenerateInitialPopulation(descendents, popSize);
+            Console.WriteLine("mean fitness: "+MeanFitness(population, popSize));
+
+            //Parada = 32 geracoes sem melhora
+            while (generationsWithoutImprovement < 100){
+                //Selecao
+                for (int i = 0; i < popSize/2; i++)
+                {
+                    pairs[i,0] = rnd.Next(0, popSize);
+                    pairs[i,1] = rnd.Next(0, popSize);
+                    while (pairs[i, 1] == pairs[i, 0]){
+                        pairs[i, 1] = rnd.Next(0, popSize);
+                    }
+                    //Console.WriteLine(pairs[i, 0] + " " + pairs[i, 1]);
+                }
+
+                //tentativa de fazer sorteio com fitness
+                /* accFitness[0] = population[0].Fitness;
+                 for (int i = 1; i < popSize; i++){
+                     totalFitness += population[i].Fitness;
+                     accFitness[i] = totalFitness;
+                    //Console.WriteLine(accFitness[i]);
+                 }
+
+                /*for (int i = 0; i < popSize/2; i++){
+                     sorteio = rnd.Next(0, totalFitness);
+                     if (Enumerable.Range(0, accFitness[0]).Contains(sorteio)){
+                         pairs[i, 0] = 0;
+                     } else{
+                         for (int j = 1; j < popSize; j++){
+                             if (Enumerable.Range(accFitness[j - 1]+1, accFitness[j]).Contains(sorteio)){
+                                 pairs[i, 0] = j;
+                                 break;
+                             }
+                         }
+                     }
+
+                     sorteio = rnd.Next(0, totalFitness);
+                     if (Enumerable.Range(0, accFitness[0]).Contains(sorteio))
+                     {
+                         pairs[i, 1] = 0;
+                     }
+                     else
+                     {
+                         for (int j = 1; j < popSize; j++)
+                         {
+                             if (Enumerable.Range(accFitness[j - 1] + 1, accFitness[j]).Contains(sorteio))
+                             {
+                                 pairs[i, 1] = j;
+                                 break;
+                             }
+                         }
+                     }
+                 }*/
+
+                //cruzamento
+
+                for (int i = 0; i < (popSize/2)-1; i++){
+                    if (rnd.NextDouble()*(100-0)+0 <= 80)
+                    {
+                        descendents[i].Gene1        = population[pairs[i, 0]].Gene1;
+                        descendents[i].Gene2        = population[pairs[i, 1]].Gene2;
+                        descendents[i + 1].Gene1    = population[pairs[i, 1]].Gene1;
+                        descendents[i + 1].Gene2    = population[pairs[i, 0]].Gene2;
+                    }
+                    //mutacao
+                    for (int j = 0; j < descendents[i].Size/2; j++){
+                        if (rnd.NextDouble()*(100-0)+0<=2) {
+                            descendents[i].Gene1[j]     = !descendents[i].Gene1[j];
+                            descendents[i].Gene2[j]     = !descendents[i].Gene2[j];
+                            descendents[i+1].Gene1[j]   = !descendents[i+1].Gene1[j];
+                            descendents[i+1].Gene2[j]   = !descendents[i + 1].Gene2[j];
+                        }
+                    }
+                }
+
+                for (int i = 0; i < popSize; i++)
+                {
+                    descendents[i].Evaluate();
+                    population[i].Evaluate();
+                }
+
+                generations++;
+                if (MeanFitness(descendents, popSize) < MeanFitness(population, popSize))
+                    generationsWithoutImprovement++;
+                else
+                {
+                    for (int i = 0; i < popSize; i++)
+                    {
+                        population[i] = descendents[i];
+                    }
+                    //population = descendents;                
+                }
+                
+            }
+
+            Console.WriteLine("mean fitness (desc): " + MeanFitness(descendents, popSize));
+
+            /*Cromosome cromosome = new Cromosome();
             cromosome.Gene1 = new BitArray(256);
             cromosome.Gene2 = new BitArray(256);
-            
             //Populacao inicial aleatoria + Fitness inicial
             cromosome.Initialize();
-
-            //Console.WriteLine(cromosome.Fitness);
-            //PrintGene(cromosome.Gene1, 32);
-            //PrintGene(cromosome.Gene2, 32);
+            Console.WriteLine(cromosome.Fitness);
+            PrintGene(cromosome.Gene1, 32);
+            PrintGene(cromosome.Gene2, 32);*/
 
             Console.WriteLine("Pressione uma tecla");
             Console.ReadKey();
 
         }
 
+
+        public static void GenerateInitialPopulation(List<Cromosome> population, int popSize)
+        {
+            for (int i = 0; i < popSize; i++)
+            {
+                population.Add(new Cromosome(512));
+                population[i].Initialize();
+            }
+        }
+
+        public static double MeanFitness(List<Cromosome> pop, int popSize)
+        {
+            double mean = 0;
+            for (int i = 0; i < popSize; i++)
+            {
+                mean += pop[i].Fitness;
+            }
+            return mean/popSize;
+        }
+        
         public static void PrintGene(BitArray gene, int myWidth)
         {
             int i, width=myWidth;
